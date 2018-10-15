5818ed521688c72af83636b10c601268f989ee3a
Dean Troyer <dtroyer@gmail.com>
2018-10-15 17:17:35 -0500

    Add DevStack module to install tsconfig
    The first of a number of anticipated bits...
    
    Change-Id: I829ca465ce3001542bba240955af7ffb0c824a8b
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

---
+++ devstack/plugin.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# devstack/plugin.sh
+# Triggers stx_update specific functions to install and configure stx_update
+
+# Dependencies:
+#
+# - ``functions`` file
+# - ``DATA_DIR`` must be defined
+
+# ``stack.sh`` calls the entry points in this order:
+#
+echo_summary "stx-update devstack plugin.sh called: $1/$2"
+source $DEST/stx-update/devstack/lib/stx-update
+
+# check for service enabled
+if is_service_enabled stx-update; then
+    if [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # Perform installation of source
+        echo_summary "Installing stx-update"
+        # install_update
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # Configure after the other layer 1 and 2 services have been configured
+        echo_summary "Configuring update"
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        # Initialize and start the service
+        echo_summary "Initializing and start stx-update"
+        # init_update
+        # start_update
+    elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
+        # do sanity test
+        echo_summary "do test-config"
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # Shut down services
+        echo_summary "Stop service"
+        # stop_update
+        :
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        echo_summary "Clean stx-update"
+        # cleanup_update
+        :
+    fi
+fi
