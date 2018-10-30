62abfa4787ff405919f122775aef1f050c27b096
Dean Troyer <dtroyer@gmail.com>
2018-10-30 08:47:32 -0500

    Add a minimal DevStack job
    This is just enough DevStack to test the plugin.
    
    Depends-On: https://review.openstack.org/613973
    Change-Id: I6e34c3c2e400235259cd13cdc7b45ea4baccd4bc
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -3,29 +3,20 @@
 # devstack/plugin.sh
 # Triggers stx_update specific functions to install and configure stx_update
 
-# Dependencies:
-#
-# - ``functions`` file
-# - ``DATA_DIR`` must be defined
-
-# ``stack.sh`` calls the entry points in this order:
-#
 echo_summary "stx-update devstack plugin.sh called: $1/$2"
-source $DEST/stx-update/devstack/lib/stx-update
 
 # check for service enabled
 if is_service_enabled stx-update; then
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
         # Perform installation of source
-        echo_summary "Installing stx-update"
-        # install_update
-
+        echo_summary "Install stx-update"
+        install_update
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
-        echo_summary "Configuring update"
+        echo_summary "Configure stx-update"
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize and start the service
-        echo_summary "Initializing and start stx-update"
+        echo_summary "Initialize and start stx-update"
         # init_update
         # start_update
     elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
@@ -35,14 +26,12 @@ if is_service_enabled stx-update; then
 
     if [[ "$1" == "unstack" ]]; then
         # Shut down services
-        echo_summary "Stop service"
+        echo_summary "Stop stx-update services"
         # stop_update
-        :
     fi
 
     if [[ "$1" == "clean" ]]; then
         echo_summary "Clean stx-update"
         # cleanup_update
-        :
     fi
 fi
