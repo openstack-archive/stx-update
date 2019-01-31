e6503af8c23562ed826225d8500dd143ef200e5b
Kristal Dale <kristal.dale@intel.com>
2019-01-31 15:38:33 -0800

    Update config for release notes to include project name
    Update conf.py for release notes to include the project
    variable, set to the project name. This is so the string
    above the left nav renders the project name.
    
    Story: 2004900
    Task: 29232
    
    Change-Id: I2c60b94b61884a1f366b57d197b69dcee38761ef
    Signed-off-by: Kristal Dale <kristal.dale@intel.com>

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -46,6 +46,8 @@ source_suffix = '.rst'
 # The master toctree document.
 master_doc = 'index'
 
+project = 'stx-update'
+
 # Release notes are version independent, no need to set version and release
 release = ''
 version = ''
